655b4a8ae9169f6b0d86484b3e2c4af92b3372a8
laisfaustino <laisfaustino@hotmail.com>
2023-06-01 11:46:28 -0300

    Parte Final

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://blogpessoal-huce.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
     export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
